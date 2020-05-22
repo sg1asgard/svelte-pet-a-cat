cd2b5b86d9e94ccea07ce54aed00314c9e588654
sg1asgard <sg1asgard@outlook.com>
2020-05-22 14:04:26 +0300

    fixed counter

--- src/AppTimer.svelte
+++ src/AppTimer.svelte
@@ -5,15 +5,24 @@
     let secondsLeft = totalSeconds;
     let isRunning = false;
 
+    // Start Timer to pet a Cat.
     function startTimer() {
+        // Set timer running as true, use it to disable the button while true
         isRunning = true;
+
+        // check if the timer is at 0 seconds
         if (secondsLeft == 0) {
+            // if yes then reset the timer
             secondsLeft = totalSeconds;
         }
+
+        // run then timer countdown
         const timer = setInterval(() => {
             secondsLeft -= 1;
             if (secondsLeft == 0) {
+                // stop the timer running
                 clearInterval(timer);
+                // enable the button for click
                 isRunning = false;
             }
         }, 1000);
