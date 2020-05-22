a72f69df288637c73a28ec4cf4547da66f9de993
sg1asgard <sg1asgard@outlook.com>
2020-05-22 15:48:51 +0300

    Ading BlueCSs Grid

--- src/AppHowTo.svelte
+++ src/AppHowTo.svelte
@@ -1,9 +1,43 @@
 <style>
     /* your styles go here */
+    .pets-container {
+        margin-top: 32px;
+        max-width: 560px;
+    }
     img {
-        max-width: 82%;
+        display: inline-block;
+        border-radius: 4px;
+        max-width: 100%;
     }
 </style>
-<div>
-    <img src="tuxedo-cats.jfif" alt="Tuxedo Cats" />
+<div class="pets-container">
+    <div bp="grid 4">
+        <div>
+            <img src="cats/cat-to-pet-01.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-02.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-03.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-04.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-05.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-06.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-07.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-08.jpg" alt="Pet a Cat" />
+        </div>
+        <div>
+            <img src="cats/cat-to-pet-09.jpg" alt="Pet a Cat" />
+        </div>
+    </div>
 </div>
