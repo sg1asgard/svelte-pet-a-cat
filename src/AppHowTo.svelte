745a2fd1f4aebefde391136efd7a6eaddb5f2e65
sg1asgard <sg1asgard@outlook.com>
2020-05-22 16:17:38 +0300

    Adding a sound file and refactoring the code a bit

--- src/AppHowTo.svelte
+++ src/AppHowTo.svelte
@@ -13,31 +13,31 @@
 <div class="pets-container">
     <div bp="grid 4">
         <div>
-            <img src="cats/cat-to-pet-01.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-01.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-02.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-02.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-03.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-03.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-04.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-04.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-05.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-05.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-06.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-06.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-07.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-07.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-08.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-08.jpg" alt="Pet a Cat" />
         </div>
         <div>
-            <img src="cats/cat-to-pet-09.jpg" alt="Pet a Cat" />
+            <img src="cat-images/cat-to-pet-09.jpg" alt="Pet a Cat" />
         </div>
     </div>
 </div>
